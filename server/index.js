7338177a2d452cd5457159b3304ef13f948d4f13
Dima Akzhigitov <trololoshka609@gmail.com>
2021-02-11 14:00:35 +0300

    Add npm and npm debug

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    console.log('request', req.url);
+
+    let file = 'index.html';
+
+    if (req.url !== '/') {
+        file = req.url.replace('/', '');
+    }
+
+    fs.readFile(`public/${file}`, (err, data) => {
+        if (err) {
+            console.log('error', err);
+
+            res.end();
+
+            return;
+
+        }
+
+        res.write(data);
+
+        res.end();
+    })
+});
+
+server.listen(3000);
